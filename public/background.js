8991c841748696c91b2b0a422827362fead276b6
Scronheim <scronheim@yandex.ru>
2024-04-12 17:59:47 +0500

    тест

--- public/background.js
+++ public/background.js
@@ -1,1 +1,5 @@
-console.log("Background - service worker (updated)");chrome.runtime.onInstalled.addListener(()=>{});
+console.log("Background - service worker (updated)"); chrome.runtime.onInstalled.addListener(() => { });
+chrome.tabs.onActivated.addListener(async e => {
+  const tab = await chrome.tabs.get(e.tabId)
+  console.log(tab)
+});
